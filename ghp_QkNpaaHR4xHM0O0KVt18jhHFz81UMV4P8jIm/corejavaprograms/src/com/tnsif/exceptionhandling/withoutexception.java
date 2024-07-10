52779bcd06ae2733641ee66d9db841953c6dc259
sai kiran <sai kiran@LAPTOP-1ALJTU0F.Dlink>
2024-07-10 12:11:54 +0530

    commit message

---
+++ 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/corejavaprograms/src/com/tnsif/exceptionhandling/withoutexception.java
@@ -0,0 +1,9 @@
+package com.tnsif.exceptionhandling;
+
+public class withoutexception {
+	public static void main(String[] args) {
+		int d=5;
+		
+	}
+
+}
---
+++ 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/corejavaprograms/src/com/tnsif/finallyblock/Test3.java
@@ -0,0 +1,16 @@
+package com.tnsif.finallyblock;
+
+public class Test3 {
+	public static void main(String[] args) {
+		try {
+			System.out.println(100/0);
+		}
+		catch(NullPointerException e) {
+			System.out.println("welcome");
+		}
+		finally {
+			System.out.println("hi");
+		}
+	}
+
+}
